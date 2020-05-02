cec4e355424555b8393780f847bad0afb6e66a8d
Chakresh <chakresh0108@gmail.com>
2020-05-03 01:33:58 +0530

    Add files via upload

---
+++ Solution.java
@@ -0,0 +1,28 @@
+package com.example.demo.javaprogram;
+
+/**
+ * 
+ *You're given strings J representing the types of stones that are jewels, and S representing the stones you have.  Each character in S is a type of stone you have.  You want to know how many of the stones you have are also jewels.
+
+The letters in J are guaranteed distinct, and all characters in J and S are letters. Letters are case sensitive, so "a" is considered a different type of stone from "A".
+ *
+ */
+
+public class Solution  {
+	public int numJewelsInStones(String J, String S) {
+
+		int count = 0;
+		char chj[] = J.toCharArray();
+		char ch[] = S.toCharArray();
+		for (int i = 0; i < ch.length; i++) {
+			for (int j = 0; j < chj.length; j++) {
+				if (ch[i] == chj[j]) {
+					count++;
+				}
+			}
+
+		}
+		return count;
+	}
+
+}
